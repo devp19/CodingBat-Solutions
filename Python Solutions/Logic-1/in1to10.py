2ddf003e70087dec4de526fbee1f5b87f8fa22f1
Dev Patel <devptl841806@gmail.com>
2024-01-24 09:15:02 -0500

    Create in1to10.py

---
+++ Python Solutions/Logic-1/in1to10.py
@@ -0,0 +1,12 @@
+def in1to10(n, outside_mode):
+  if outside_mode:
+    if n <= 1 or n >= 10:
+      return True
+    else:
+      return False
+      
+  elif (outside_mode == False) and  (1 <= n <= 10):
+    return True
+    
+  else:
+    return False
