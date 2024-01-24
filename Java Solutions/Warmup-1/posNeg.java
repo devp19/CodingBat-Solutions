62cf954790eae630b39a39bf5cbe4abe810fd821
Dev Patel <devptl841806@gmail.com>
2024-01-24 09:23:19 -0500

    Create posNeg.java

---
+++ Java Solutions/Warmup-1/posNeg.java
@@ -0,0 +1,12 @@
+public boolean posNeg(int a, int b, boolean negative) {
+  
+  if(negative && a<0 && b<0){
+    return(true);
+  }
+  
+  if(!negative && (a<0 && b>=0 || a>=0 && b<0)){
+    return(true);
+  }
+  
+  return(false);
+}
